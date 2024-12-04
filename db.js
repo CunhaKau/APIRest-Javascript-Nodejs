4452b5bf4c4416c1b8dd1cdfa37b9b8b50c0cc95
Ana Claudia Cunha <ana.cscunha1@senacsp.edu.br>
2024-12-04 03:58:56 -0300

    vercel

--- db.js
+++ db.js
@@ -1,21 +1,18 @@
-// Importando o pacote mysql2
-const mysql = require('mysql2');
+const mysql = require('mysql2')
 
-// Criando a conexão com o banco de dados MySQL
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DBNAME
+const pool = mysql.createPool({
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USERNAME, 
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-// Conectando ao banco de dados
-connection.connect((err) => {
-  if (err) {
-    console.error('Erro ao conectar ao banco de dados: ' + err.stack);
-    return;
-  }
-  console.log('Conectado ao banco de dados como id ' + connection.threadId);
-});
+pool.getConnection((err, conn) => {
+    if(err) console.log(err)
+    console.log("Connected successfully")
+})
 
-module.exports = connection;
+module.exports = pool.promise();
